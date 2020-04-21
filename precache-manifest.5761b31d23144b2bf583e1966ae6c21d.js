9095abfe409c2dd1dad156ef946c41a5d75386df
wardahmad <whitelight43@hotmail.com>
2020-04-21 19:30:01 +0300

    Updates

---
+++ precache-manifest.5761b31d23144b2bf583e1966ae6c21d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805039a1f0cc2288d2f8a273bcfecc3a",
+    "url": "/snake-game-React/index.html"
+  },
+  {
+    "revision": "1dbed7473fbd38f493f9",
+    "url": "/snake-game-React/static/css/main.5a9c5b69.chunk.css"
+  },
+  {
+    "revision": "5928405a567a2a370eca",
+    "url": "/snake-game-React/static/js/2.f7d4ba49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game-React/static/js/2.f7d4ba49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbed7473fbd38f493f9",
+    "url": "/snake-game-React/static/js/main.0b79a826.chunk.js"
+  },
+  {
+    "revision": "75e7b3cfe01526215085",
+    "url": "/snake-game-React/static/js/runtime-main.c2df1f02.js"
+  }
+]);
